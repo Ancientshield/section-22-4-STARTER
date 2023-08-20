9f37cd95efde8ba660843698e4e7522ab685c02e
Ancientshield <jacklin2100@gmail.com>
2023-08-20 17:18:16 +0800

    separate EmojiInput.vue from App.vue

---
+++ src/components/EmojiInput.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="input-group">
+		<input type="text" class="form-control" readonly />
+		<button class="btn btn-outline-secondary" type="button">Button</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'EmojiImput',
+	};
+</script>
